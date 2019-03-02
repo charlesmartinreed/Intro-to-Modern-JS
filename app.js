ce45c061d815e750bd48cc731bbd6455f35dd86b
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-01 18:59:25 -0600

    Basic demonstration of built-in constructors in JS.

--- app.js
+++ app.js
@@ -1,35 +1,58 @@
-// OBJECT LITERALS
+// BUILT-IN CONSTRUCTORS
+// primitive types like String, Number, etc also have constructors but it isn't advisible to utilize these types in this way
 
-// const brad = {
-// 	name: 'Brad',
-// 	age: 30
-// }
+// STRING CONSTRUCTOR
+const name1 = 'Jeff';
+const name2 = new String('Jeff');
 
-// console.log(brad);
-// console.log(brad.age);
+//console.log(name1);
+//console.log(name2); //note that this logs this as an object, which each letter comprising a different property
 
-// CREATING A PERSON CONSTRUCTOR - ES5 era
-function Person(name, dob) {
-	// this refers to the current instance of an object
-	this.name = name;
-	//this.age = age;
-	this.birthday = new Date(dob);
-	this.calculateAge = function(){
-		//current date - number of milliseconds since 1/1/1970 and user birthday
-		console.log(this.birthday.getFullYear())
-		const diff = Date.now() - this.birthday.getTime();
-		const ageDate = new Date(diff);
-		return Math.abs(ageDate.getUTCFullYear() - 1970);
+//we can also add properties to the name2 string as it's being treated as an object
+// name2.foo = 'bar';
+// console.log(name2);
 
-	}
-	//console.log(this)
+//  === can be a problem with this constructed String objects because the type is object, not String
+if(name2 === 'Jeff') { //value matches, type doesn't
+	console.log('YES');
+} else {
+	console.log('NO');
 }
-const brad = new Person('Brad', '9-10-1981');
-console.log(brad.calculateAge());
 
-// INSTANTIATING A PERSON
-//const userA = new Person('Brad', 15);
-//const userB = new Person('David', 29);
+// NUMBER CONSTRUCTOR
+const num1 = 5;
+const num2 = new Number(5);
+console.log(typeof num2);
 
-// using 'this' outside, in the global scope for instance, gives us the window object.
-// this.alert('\'this\' refers to the window at global scope')
+// BOOLEAN CONSTRUCTOR
+const bool1 = true;
+const bool2 = new Boolean(true);
+console.log(typeof bool2);
+
+// FUNCTION CONSTRUCTOR
+const getSum1 = function(x, y){
+	return x + y;
+}
+console.log(getSum1(5,10));
+
+const getSum2 = new Function('x', 'y', `return x + y`);
+console.log(getSum2(7,2));
+
+// OBJECT CONSTRUCTOR
+const john = {name: 'John'};
+console.log(john);
+
+const john2 = new Object({name: 'John'})
+console.log(john2);
+
+// ARRAY CONSTRUCTOR
+const array1 = [1, 2, 3, 4];
+const array2 = new Array(1,2,3,4);
+console.log(array2);
+
+// REGULAR EXPRESSION
+const regEx1 = /\w+/; //word character that appears 1 or more times
+const regEx2 = new RegExp('\\w+'); //using the constructor, you have to escape your slashes, which you don't when using the regular means of defining an reg ex in JS
+
+console.log(regEx1);
+console.log(regEx2);
