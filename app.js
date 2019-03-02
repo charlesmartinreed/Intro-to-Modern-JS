9f8849caa3d0a2342283d55789f54ee99a0b0cff
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-01 19:43:51 -0600

    Basic demonstration of Object.create() in ES5 JS.

--- app.js
+++ app.js
@@ -1,43 +1,30 @@
-// INHERITING PROTOTYPES
+// Object.create creates a new object with an existing object as its prototype
 
-// PERSON
-function Person(firstName, lastName) {
-	this.firstName = firstName;
-	this.lastName = lastName;
+const personPrototypes = {
+	greeting: function() {
+		return `Hello, ${this.firstName} ${this.lastName}`
+	},
+	getsMarried: function(newLastName) {
+		this.lastName = newLastName;
+	}
 }
 
-// Greeting prototype method
-Person.prototype.greeting = function() {
-	return `Hello there, from ${this.firstName} ${this.lastName}!`;
-}
+const danae = Object.create(personPrototypes);
 
-// CREATE A PERSON INSTANCE
-const person1 = new Person('James', 'Brown');
-console.log(person1.greeting());
+// we can add unique properties to our object
+danae.firstName = 'Danae';
+danae.lastName = 'Jenkins';
+danae.age = 30;
 
-// CUSTOMER CONSTRUCTOR
-function Customer(firstName, lastName, phone, membership) {
-	// call is a function that allows us to call another function, from the current context
-	Person.call(this, firstName, lastName); //this of this like super.init
-	this.phone = phone;
-	this.membership = membership;
-}
+danae.getsMarried('Spicoli');
+console.log(danae.greeting());
 
-// INHERITING THE PERSON PROTOTYPE METHODS
-Customer.prototype = Object.create(Person.prototype);
+// alternate syntax
+const charles = Object.create(personPrototypes, {
+	firstName: {value: 'Charles'},
+	lastName: {value: 'Reed'},
+	age: {value: 36}
+});
 
-// MAKE customer.prototype return as Customer()
-Customer.prototype.constructor = Customer;
-
-// CREATE A CUSTOMER
-const customer1 = new Customer('Beavis', 'Maron', '555-555-1111', 'standard');
-console.log(customer1);
-//console.log(customer1.greeting()); //this errors out because we're not yet inheriting from Person
-console.log(customer1.greeting()); //even though customer is a CUSTOMER, not a PERSON, this now works because we're sharing a prototype
-
-// we can also create custom implementations of prototype methods for our instances
-Customer.prototype.greeting = function() {
-	return `Hello there, from ${this.firstName} ${this.lastName}! Thank you for being a part of our family!`;
-}
-
-console.log(customer1.greeting()); // this will use our newly defined greeting
+console.log(charles);
+console.log(charles.greeting());
