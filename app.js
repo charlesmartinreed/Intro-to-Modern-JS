b030599a59d5772ac0019ef703cd4a796ea18e0e
charlesmartinreed <charlesmartinreed@icloud.com>
2019-03-01 19:54:01 -0600

    Basic demonstration of ES6 class/constructor syntax and static functions.

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-// Object.create creates a new object with an existing object as its prototype
+// class, constructor, methods/properties inside of class - similar to other C style languages
 
-const personPrototypes = {
-	greeting: function() {
-		return `Hello, ${this.firstName} ${this.lastName}`
-	},
-	getsMarried: function(newLastName) {
-		this.lastName = newLastName;
+class Person {
+	constructor(firstName, lastName, dob) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.birthday = new Date(dob);
+	}
+
+	//class methods are added to the prototype
+	greeting() {
+		return `Hello, ${this.firstName} ${this.lastName}!`
+	}
+
+	calculateAge() {
+		const diff = Date.now() - this.birthday.getTime();
+		const ageDate = new Date(diff);
+		return Math.abs(ageDate.getUTCFullYear() - 1970);
+	}
+
+	getsMarried(newLastName) {
+		this.lastName = newLastName
+	}
+
+	// static methods are used without instantiating an object instance
+	static addNumbers(x, y) {
+		return x + y;
 	}
 }
 
-const danae = Object.create(personPrototypes);
+// object creation
+const tyson = new Person('Mike', 'Tyson', '11-13-1965');
 
-// we can add unique properties to our object
-danae.firstName = 'Danae';
-danae.lastName = 'Jenkins';
-danae.age = 30;
+ //note how we still have the __proto__, which instead has a constructor property. That's why ES6 is described as syntactic sugar, it doesn't actually change the underlying functionality.
 
-danae.getsMarried('Spicoli');
-console.log(danae.greeting());
+console.log(tyson.getsMarried('King'));
+console.log(tyson);
+//console.log(tyson.greeting());
+//console.log(tyson.calculateAge());
 
-// alternate syntax
-const charles = Object.create(personPrototypes, {
-	firstName: {value: 'Charles'},
-	lastName: {value: 'Reed'},
-	age: {value: 36}
-});
-
-console.log(charles);
-console.log(charles.greeting());
+// using our static function - note that we call it with the class name
+console.log(Person.addNumbers(5,10));
